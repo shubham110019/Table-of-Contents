5e03fae671749cdc42dacbf256a68758ca682d70
shubham ralli <sralli73@gmail.com>
2023-01-29 14:58:32 +0530

    Update ez-main.js

--- ez-main.js
+++ ez-main.js
@@ -1,28 +1,22 @@
-jQuery('h2').each(function(i) {
-    var myH2 =  jQuery(this).text();
-    myH2 = myH2.replace(/ /g, "-").toLowerCase().replace(/,/g, "").replace(/\?/g, "").replace(/\./g, "");
-    jQuery(this).prepend("<span class='ez-data' id='"+myH2+"'></span>");
-});
-
-jQuery('h3').each(function(i) {
-    var myH3 =  jQuery(this).text();
-    myH3 = myH3.replace(/ /g, "-").toLowerCase().replace(/,/g, "").replace(/\?/g, "").replace(/\./g, "");
-    jQuery(this).prepend("<span class='ez-data' id='"+myH3+"'></span>");
-});
-
-jQuery('h4').each(function(i) {
-    var myH4 =  jQuery(this).text();
-    myH4 = myH4.replace(/ /g, "-").toLowerCase().replace(/,/g, "").replace(/\?/g, "").replace(/\./g, "");
-    jQuery(this).prepend("<span class='ez-data' id='"+myH4+"'></span>");
+// find heading and add span with id
+jQuery('h2, h3, h4').each(function(i) {
+    var myHd =  jQuery(this).text();
+    myHd = myHd.replace(/ /g, "-").toLowerCase().replace(/,/g, "").replace(/\?/g, "").replace(/\./g, "");
+    jQuery(this).prepend("<span class='ez-data' id='"+myHd+"'></span>");
 });
 
 jQuery('#ez-table').prepend("<div class='ez-table'><p>Table of Contents</p><div class='ez-side-menu' id='ez-side-menu'><ul></ul></div></div>");
 
+// find heading and add to side menu
 jQuery('h2, h3').each(function(i) {
     var mydata = jQuery(this).text();
     var mydatalink = jQuery(this).find('span').attr('id')
-    jQuery('.ez-side-menu ul').prepend("<li><a href='#"+mydatalink+"'>"+mydata+"</a></li>");
-    
+    jQuery('.ez-side-menu ul').prepend("<li class='ez-"+i+"'><a href='#"+mydatalink+"'>"+mydata+"</a></li>");
+
 });
 
-
+// side menu li add height
+jQuery('#ez-side-menu ul li').each(function(index) {
+    jQuery(this).attr("data-id", "" + index + "");
+    jQuery(this).attr("data-height", "" + jQuery(this).height() + "");
+});
