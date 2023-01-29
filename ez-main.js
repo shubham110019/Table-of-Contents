155c898957fd878119a20bc0ae85c3d55f37537b
shubham ralli <sralli73@gmail.com>
2023-01-29 15:10:22 +0530

    Update ez-main.js

--- ez-main.js
+++ ez-main.js
@@ -8,15 +8,15 @@ jQuery('h2, h3, h4').each(function(i) {
 jQuery('#ez-table').prepend("<div class='ez-table'><p>Table of Contents</p><div class='ez-side-menu' id='ez-side-menu'><ul></ul></div></div>");
 
 // find heading and add to side menu
-jQuery('h2, h3').each(function(i) {
+jQuery.fn.reverse = [].reverse;
+jQuery('h2, h3').reverse().each(function(i) {
     var mydata = jQuery(this).text();
     var mydatalink = jQuery(this).find('span').attr('id')
-    jQuery('.ez-side-menu ul').prepend("<li class='ez-"+i+"'><a href='#"+mydatalink+"'>"+mydata+"</a></li>");
+    jQuery('.ez-side-menu ul').prepend("<li class='ez-list ez-"+i+"'><a href='#"+mydatalink+"'>"+mydata+"</a></li>");
 
 });
 
 // side menu li add height
 jQuery('#ez-side-menu ul li').each(function(index) {
-    jQuery(this).attr("data-id", "" + index + "");
     jQuery(this).attr("data-height", "" + jQuery(this).height() + "");
 });
