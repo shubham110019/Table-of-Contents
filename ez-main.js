1822b4fc85155906915163726934467c11d1fa26
shubham ralli <sralli73@gmail.com>
2023-01-29 15:37:56 +0530

    Update ez-main.js

--- ez-main.js
+++ ez-main.js
@@ -1,3 +1,14 @@
+
+// table title 
+var tbTitle = jQuery('#ez-table').data('title');
+
+// table show
+var tbEvent= jQuery('#ez-table').data('event');
+
+if(tbEvent == undefined)
+{ tbEvent = 'h2';}
+
+
 // find heading and add span with id
 jQuery('h2, h3, h4').each(function(i) {
     var myHd =  jQuery(this).text();
@@ -5,11 +16,14 @@ jQuery('h2, h3, h4').each(function(i) {
     jQuery(this).prepend("<span class='ez-data' id='"+myHd+"'></span>");
 });
 
-jQuery('#ez-table').prepend("<div class='ez-table'><p>Table of Contents</p><div class='ez-side-menu' id='ez-side-menu'><ul></ul></div></div>");
+
+
+
+jQuery('#ez-table').prepend("<div class='ez-table'><span id='ex-title'>"+tbTitle+"</span><div class='ez-side-menu' id='ez-side-menu'><ul></ul></div></div>");
 
 // find heading and add to side menu
 jQuery.fn.reverse = [].reverse;
-jQuery('h2, h3').reverse().each(function(i) {
+jQuery(tbEvent).reverse().each(function(i) {
     var mydata = jQuery(this).text();
     var mydatalink = jQuery(this).find('span').attr('id')
     jQuery('.ez-side-menu ul').prepend("<li class='ez-list ez-"+i+"'><a href='#"+mydatalink+"'>"+mydata+"</a></li>");
